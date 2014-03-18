f12415450e4c85aabcf33f1ce2fff9a8872b4893
Jonathan Rivera - Picerne <jrivera@picernefl.com>
2014-03-18 17:07:17 -0400

    Setup Map and remove hardcoded HTML

--- templates/content-amenities.php
+++ templates/content-amenities.php
@@ -13,52 +13,5 @@
  
 <?php endwhile; ?>
 
-<!-- Begin The Amenities Listing -->
-  <div class="amenities-wrapper">
 
-	<h4>Community Amenities</h4>
-
-    <div class="amenities-item span12">
-    <div class="span6">
-    <img src="<?php echo get_bloginfo('url');?>/assets/parkside-point-amenities.jpg" alt="Parkside Point Aparment Homes - Community Amenities" width="300" height="200" class="alignleft size-medium" />
-    </div>
-    <div class="span6">
-       <ul class="amenities-list-items">
-            <li>Access to Public Transportation</li>
-            <li>Beautiful Landscaping</li>
-            <li>Business Center</li>
-            <li>Children's Play Area</li>
-            <li>Clubhouse</li>
-            <li>Copy &amp; Fax Services</li>
-            <li>Easy Access to Freeways</li>
-            <li>Easy Access to Shopping</li>
-        </ul>
-      </div>
-    </div>
-    
-    
-    <h4>Apartment Amenities</h4>
-    
-    <div class="amenities-item span12">
-    <div class="span6">
-    <img src="<?php echo get_bloginfo('url');?>/assets/parkside-point-apt-amenities.jpg" alt="Parkside Point Aparment Homes - Apartment Amenities" width="300" height="200" class="alignleft size-medium" />
-    </div>
-    <div class="span6">
-       <ul class="amenities-list-items">
-       		 <li>Air Conditioning</li>
-            <li>All Electric Kitchen</li> 
-            <li>Breakfast Bar</li> 
-            <li>Cable Ready</li>
-            <li>Carpeted Floors</li>
-            <li>Ceiling Fan(s)</li>            
-            <li>Central Air/Heating</li>  
-            <li>Dishwasher</li>  
-            <li>Garbage Disposal</li>
-            <li>Mini Blinds</li>
-        </ul>
-      </div>
-    </div>
-   
-
-  </div> <!-- end Amenities Wrapper -->
 </div> <!-- end row-fluid -->
--- templates/content-contact.php
+++ templates/content-contact.php
@@ -1,54 +1,14 @@
 <div class="row-fluid page-main-wrap">
-<?php while (have_posts()) : the_post(); ?>
-  
-  <div class="contact-left span6">
-  <?php if (has_post_thumbnail()){ //Post Thumbnail check?>
-  
-  
-  <figure class="pull-left feat-thumb">
-   <?php get_template_part('templates/featured-image'); //Display the Featured image of the home page ?> 
-  </figure>
-  
-  <?php } ?>
-	<h3>Want us to get back to you?</h3>
+<?php while (have_posts()) : the_post();
+
+
+	   if ( shortcode_exists( 'display_gmap' ) ) { 
+			 echo do_shortcode('[display_gmap]');
+	   } 
+?> 
   <?php the_content(); ?>
   <?php wp_link_pages(array('before' => '<nav class="pagination">', 'after' => '</nav>')); ?>
-  </div> <!-- end span6 -->
   
-  <div class="contact-right span6">
-   <div class="connect-wrap span9">
-     <h2>Connect With us</h2>
-        <div class="media">
-          <a class="pull-left" href="#">
-            <img class="media-object img-circle" data-src="holder.js/64x64" src="<?php echo get_bloginfo('url');?>/assets/phone-icon.png" alt="Call Parkside Point Apartment Homes"/>
-          </a>
-          <div class="media-body">
-            <h5 class="media-heading">Give us a call</h5>
-            <p>(832) 200-9947</p>
-          </div>
-        </div>     
-        <div class="media">
-          <a class="pull-left" href="#">
-            <img class="media-object img-circle" data-src="holder.js/64x64" src="<?php echo get_bloginfo('url');?>/assets/mail-icon.png" alt="Email Parkside Point Apartment Homes"/>
-          </a>
-          <div class="media-body">
-            <h5 class="media-heading">Email</h5>
-            <p>Send us an email: <a href="mailto:parkside_p@picernefl.com">parkside_p@picernefl.com</a></p>
-          </div>
-        </div>    
-        <!-- <div class="media">
-          <a class="pull-left" href="#">
-            <img class="media-object img-circle" data-src="holder.js/64x64" src="<?php // echo get_stylesheet_directory_uri();?>/assets/img/facebook-icon-1.png"/>
-          </a>
-         <div class="media-body">
-            <h5 class="media-heading">Facebook</h5>
-            <p>Keep up with the latest news and events on our <a href="https://www.facebook.com/oasisatwekiva" target="_blank">Facebook Page</a></p>
-          </div>
-        </div>   -->          
-              
-     
-   </div>
-  </div>
   
 <?php endwhile; ?>
 
